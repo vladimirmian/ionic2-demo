fd258782cde504fb1d3ec39878a234b76d96963f
vladimirmian <1909582621@qq.com>
2017-07-30 22:45:55 +0800

    修改登录的请求

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+	selector: 'page-login',
+	templateUrl: 'login.html'
 })
 export class LoginPage {
+	private loginParams: object = {
+		companyNum: "",//企业编号
+		userName: "",//用户名,
+		passWord: ""//密码
+	}
+	constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+	}
+	login(){
+		console.log(this.loginParams)
+	}
 }
