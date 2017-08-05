a3b1f34d226e5b3c2f41fd594dfa386a9fb755ec
vladimirmian <1909582621@qq.com>
2017-08-05 16:48:41 +0800

    写完国内机票的查询静态页

--- src/modules/inquire/pages/Domestic/domestic.ts
+++ src/modules/inquire/pages/Domestic/domestic.ts
@@ -1,13 +1,30 @@
 import { NavController } from 'ionic-angular';
 import { Component, OnInit } from '@angular/core';
 @Component({
-    selector:"inquire-domestic",
-    templateUrl:"./domestic.html"
+    selector: "inquire-domestic",
+    templateUrl: "./domestic.html"
 })
-export class DomesticPage implements OnInit{
-    private isOneWay:boolean;
-    constructor(public navCtrl:NavController){
-
+export class DomesticPage implements OnInit {
+    private isOneWay: boolean;
+    chats: any[];
+    constructor(public navCtrl: NavController) {
+        this.chats = [
+            {
+                img: './assets/avatar-cher.png',
+                name: 'Cher',
+                message: 'Ugh. As if.',
+                time: '9:38 pm'
+            }, {
+                img: './assets/avatar-dionne.png',
+                name: 'Dionne',
+                message: 'Mr. Hall was way harsh.',
+                time: '8:59 pm'
+            }, {
+                img: './assets/avatar-murray.png',
+                name: 'Murray',
+                message: 'Excuse me, "Ms. Dione."',
+                time: 'Wed'
+            }];
     }
     ngOnInit() {
         //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
