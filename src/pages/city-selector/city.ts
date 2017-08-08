53afa78adc711835c5a04df634da259023e6960a
vladimirmian <1909582621@qq.com>
2017-08-08 18:25:33 +0800

    上传城市列表

--- src/pages/city-selector/city.ts
+++ src/pages/city-selector/city.ts
@@ -1,25 +1,31 @@
 import { CityData } from './city_list';
-import { ViewController ,NavParams} from 'ionic-angular';
-import { Component } from '@angular/core';
+import { ViewController, NavParams, Content } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
 @Component({
-    providers:[CityData],
-    templateUrl:"./city.html",
-    selector:'city-selector'
+    providers: [CityData],
+    templateUrl: "./city.html",
+    selector: 'city-selector'
 })
-export class CitySelector{
-    public hotelList;
-    constructor(public viewCtrl:ViewController,public navParams:NavParams,public CityData:CityData){
-        console.log('asdddd');
-        // console.log(this.CityData.getHOTELCITYLIST());
-        this.hotelList = this.CityData.getHOTELCITYLIST();
+export class CitySelector {
+    @ViewChild(CitySelector) CitySelector: Content;
+    private citylist;
+    private hotCityList;
+    private letterIndex;
+    private letterInfo;
+    private cityAllList;
+    constructor(public viewCtrl: ViewController, public navParams: NavParams, public CityData: CityData) {
+        this.citylist = this.CityData.getHOTELCITYLIST();
+        this.hotCityList = this.citylist.hot.cityList;
+        this.letterIndex = this.citylist.keys;
+        this.cityAllList = this.citylist.domestic;
     }
-    close(){
+    close() {
         this.viewCtrl.dismiss();
     }
-    onInput(e){
+    onInput(e) {
         console.log(e);
     }
-    ionCancel(e){
+    ionCancel(e) {
         console.log(e);
     }
 }
